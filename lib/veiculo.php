f6e089cf324cddbc5e61efe39e3366854049f968
Israel Cidade <israelcbj@gmail.com>
2014-05-12 15:11:31 -0300

    [FIX] insert veiculos basico

--- lib/veiculo.php
+++ lib/veiculo.php
@@ -5,6 +5,20 @@
 	#Instancia o objeto
 	$banco = new bancoveiculo();
 
+	#Declara Variaveis
+	$botao = 'Salvar';
+
+	#Trabalha com Post
+	if( isset($_POST["acao"]) && $_POST["acao"] != '' ){
+		$marca = strip_tags(trim(addslashes($_POST["marca"])));
+		$modelo = strip_tags(trim(addslashes($_POST["modelo"])));
+	
+		$SqlInsert = "Insert Into c_veiculos (marca, modelo) VALUES ('".$marca."','".$modelo."')";
+		$banco->Execute($SqlInsert);
+		$banco->RedirecionaPara('veiculo');
+	}
+
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('veiculo');
+	$Conteudo = str_replace('<%BOTAO%>',$botao,$Conteudo);
 ?>
