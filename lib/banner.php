f90a57fb38d1d36850316f6a089410f873dac1fd
Israel Cidade <israelcbj@gmail.com>
2014-05-27 16:53:39 -0300

    [FIX] tela de banners

--- lib/banner.php
+++ lib/banner.php
@@ -14,12 +14,17 @@
 		$nome = strip_tags(trim(addslashes($_POST["nome"])));
 		$foto = $_FILES["foto"];
 
-		preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
-		$caminho_foto = "arq/banners/" . $id . "teste." .$ext[1];
-		move_uploaded_file($foto["tmp_name"], $caminho_foto);
-		
+		if($botao == 'Atualizar'){
+			$SqlBanco = "Update c_veiculos SET marca = '".$marca."', modelo = '".$modelo."',anofab = '".$anofab."',anomod = '".$anomod."',padrao = '".$padrao."',titulo = '".$titulo."' where idveiculo = '".$idveiculo."' ";
+		}else{
+			preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
+			$caminho_foto = "arq/banners/" . $id . "teste." .$ext[1];
+			move_uploaded_file($foto["tmp_name"], $caminho_foto);
+			$SqlBanco = "Insert Into c_banners (nome, foto) VALUES ('".$nome."','".$caminho_foto."')";
+		}
 
-		
+		$banco->Execute($SqlBanco);
+		$banco->RedirecionaPara('inicio');
 	}
 
 	$Conteudo = $banco->CarregaHtml('banner');
