b6310dbf98a2670563d0a384c61d4feebdc9ac1b
Israel Cidade <israelcbj@gmail.com>
2014-06-27 03:05:18 -0300

    [FIX] css + js

---
+++ html/style/js/admin-js/functions.js
@@ -0,0 +1,19 @@
+// GLOBAL FUNCTIONS
+
+$.fn.centraliza = function() {
+    var l = $(this).width();
+    var a = $(this).height();
+    
+    var aVal = (a/2)-a+'px';
+    var lVal = (l/2)-l+'px';
+    
+    $(this).css({
+        'position':'fixed',
+        'top':'50%',
+        'left':'50%',
+        'width':l,
+        'height':a,
+        'margin-left':lVal,
+        'margin-top':aVal
+    });  
+};
