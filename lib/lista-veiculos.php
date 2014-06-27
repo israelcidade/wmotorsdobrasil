be887c4de92c06ffc25840a9ac380ac4dc474769
Israel Cidade <israelcbj@gmail.com>
2014-06-27 11:32:30 -0300

    [FIX] testa se esta logado

--- lib/lista-veiculos.php
+++ lib/lista-veiculos.php
@@ -5,13 +5,19 @@
 	#Instancia objeto que vai tratar o banco de dados dessa pagina
 	$banco = new bancoveiculo();
 
-	#Carrega o html de Auxilio
-	$Auxilio = $banco->CarregaHtml('itens/lista-veiculos-itens');
+	if($banco->VerificaSessao()){
 
-	#Chama funcao Lista Manual passando o Auxilio
-	$Veiculos = $banco->ListaVeiculos($Auxilio);
+		#Carrega o html de Auxilio
+		$Auxilio = $banco->CarregaHtml('itens/lista-veiculos-itens');
 
-	#Imprime Valores
-	$Conteudo = $banco->CarregaHtml('lista-veiculos');
-	$Conteudo = str_replace('<%VEICULOS%>',$Veiculos,$Conteudo);
+		#Chama funcao Lista Manual passando o Auxilio
+		$Veiculos = $banco->ListaVeiculos($Auxilio);
+
+		#Imprime Valores
+		$Conteudo = $banco->CarregaHtml('lista-veiculos');
+		$Conteudo = str_replace('<%VEICULOS%>',$Veiculos,$Conteudo);
+	}else{
+		
+		$banco->RedirecionaPara('inicio/acesso');
+	}
 ?>
