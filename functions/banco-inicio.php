7a95489c4cfd350cefe3c0bc83d1e00900afe6e6
Israel Cidade <israelcbj@gmail.com>
2014-07-08 02:54:30 -0300

    [FIX] inicio

--- functions/banco-inicio.php
+++ functions/banco-inicio.php
@@ -2,7 +2,7 @@
 	class bancoinicio extends banco{
 		
 		function BuscaUsuario($usuario,$senha){
-			$Sql = "Select * from c_usuarios where user = '".$usuario."' AND senha = '".$senha."' ";
+			$Sql = "Select * from c_usuarios where cpf = '".$usuario."' AND senha = '".$senha."' ";
 			$result = parent::Execute($Sql);
 			$num_rows = parent::Linha($result);
 			return $num_rows;
