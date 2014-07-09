51c74ce197e25de7df3fc1199417d85476588e5f
Israel Cidade <israelcbj@gmail.com>
2014-07-09 02:11:04 -0300

    [FIX] busca

--- functions/banco-busca.php
+++ functions/banco-busca.php
@@ -1,6 +1,26 @@
 <?php
 	class bancobusca extends banco{
 		#Funcao que lista os Folders
-		
+
+		function ListaResultado($Auxilio){
+			$Sql = "Select * from c_veiculos where tipo = '".."' ";
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+
+			#Monta no Html a Listagem
+			if ($num_rows){
+				while( $rs = mysql_fetch_array($result , MYSQL_ASSOC) )
+				{
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%ID%>',$rs['idveiculo'],$Linha);
+					$Linha = str_replace('<%MARCA%>',$rs['nomedamarca'],$Linha);
+					$Linha = str_replace('<%MODELO%>', $rs['modelo'], $Linha);
+					$Veiculos .= $Linha;
+				}
+			}else{
+				$mensagem = $Banco_Vazio;
+			}
+			return $Veiculos;
+		}
 	}
 ?>
--- lib/busca.php
+++ lib/busca.php
@@ -5,5 +5,13 @@
 	#Instancia o objeto
 	$banco = new bancobusca();
 
+	#Carrega o html de Auxilio
+	$Auxilio = $banco->CarregaHtml('itens/lista-busca-itens');
+
+	#Chama funcao Lista Manual passando o Auxilio
+	$Busca = $banco->ListaResultado($Auxilio);
+
+	#Imprime Valores
 	$Conteudo = $banco->CarregaHtml('busca');
+	$Conteudo = str_replace('<%BUSCA%>',$Busca,$Conteudo);
 ?>
