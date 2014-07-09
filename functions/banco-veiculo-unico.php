756c4b5c61c7f9befc53e68fc53c7731ebda7ebb
Israel Cidade <israelcbj@gmail.com>
2014-07-09 16:05:26 -0300

    [FIX] tela de veiculo unico

--- functions/banco-veiculo-unico.php
+++ functions/banco-veiculo-unico.php
@@ -1,6 +1,23 @@
 <?php
 	class bancoveiculounico extends banco{
 		#Funcao que lista os Folders
-		
+
+		function BuscaVeiculoUnico($id){
+			$Sql = "Select V.*,M.marca as nomedamarca 
+					FROM c_veiculos V
+					INNER JOIN c_marcas M where V.idveiculo = '".$id."'
+					";
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+			if($num_rows){
+				$rs = mysql_fetch_array($result , MYSQL_ASSOC);
+				$VeiculoUnico['marca'] = $rs['nomedamarca'];
+				$VeiculoUnico['modelo'] = $rs['modelo'];
+				$VeiculoUnico['anofab'] = $rs['anofab'];
+				$VeiculoUnico['anomod'] = $rs['anomod'];
+				$VeiculoUnico['padrao'] = $rs['padrao'];
+			}
+			return $VeiculoUnico;
+		}
 	}
 ?>
--- lib/veiculo-unico.php
+++ lib/veiculo-unico.php
@@ -5,5 +5,17 @@
 	#Instancia o objeto
 	$banco = new bancoveiculounico();
 
+	#Carrega o html de Auxilio
+	$Auxilio = $banco->CarregaHtml('itens/lista-veiculo-unico-itens');
+
+	#Chama funcao Lista Manual passando o Auxilio
+	$VeiculoUnico = $banco->BuscaVeiculoUnico($this->PaginaAux[0]);
+
+	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('veiculo-unico');
+	$Conteudo = str_replace('<%MARCA%>',$VeiculoUnico['marca'],$Conteudo);
+	$Conteudo = str_replace('<%MODELO%>',$VeiculoUnico['modelo'],$Conteudo);
+	$Conteudo = str_replace('<%ANOFAB%>',$VeiculoUnico['anofab'],$Conteudo);
+	$Conteudo = str_replace('<%ANOMOD%>',$VeiculoUnico['anomod'],$Conteudo);
+	$Conteudo = str_replace('<%PADRAO%>',$VeiculoUnico['padrao'],$Conteudo);
 ?>
