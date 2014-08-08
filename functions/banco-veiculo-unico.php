5cef436e9cfbef120f110c1b56d888950cfe49c5
Israel Cidade <israelcbj@gmail.com>
2014-08-07 22:53:23 -0300

    [FIX] coisas

--- functions/banco-veiculo-unico.php
+++ functions/banco-veiculo-unico.php
@@ -3,21 +3,48 @@
 		#Funcao que lista os Folders
 
 		function BuscaVeiculoUnico($id){
-			$Sql = "Select V.*,M.marca as nomedamarca 
-					FROM c_veiculos V
-					INNER JOIN c_marcas M where V.idveiculo = '".$id."'
+			$Sql = "Select V.*,M.marca as nomedamarca, F.* 
+					FROM c_veiculos V 
+					INNER JOIN c_marcas M ON V.marca = M.idmarca 
+					INNER JOIN c_fotos F ON V.idveiculo = F.idveiculo 
+					where V.idveiculo = '".$id."'
+					AND F.referencia = 0
 					";
+					
+					
 			$result = parent::Execute($Sql);
 			$num_rows = parent::Linha($result);
 			if($num_rows){
 				$rs = mysql_fetch_array($result , MYSQL_ASSOC);
-				$VeiculoUnico['marca'] = $rs['nomedamarca'];
-				$VeiculoUnico['modelo'] = $rs['modelo'];
-				$VeiculoUnico['anofab'] = $rs['anofab'];
-				$VeiculoUnico['anomod'] = $rs['anomod'];
-				$VeiculoUnico['padrao'] = $rs['padrao'];
+				$VeiculoUnico['idveiculo']   = $rs['idveiculo'];
+				$VeiculoUnico['marca']   = $rs['nomedamarca'];
+				$VeiculoUnico['modelo']  = $rs['modelo'];
+				$VeiculoUnico['anofab']  = $rs['anofab'];
+				$VeiculoUnico['anomod']  = $rs['anomod'];
+				$VeiculoUnico['caminho'] = $rs['caminho'];
+				$VeiculoUnico['padrao']  = $rs['padrao'];
 			}
 			return $VeiculoUnico;
+		}
+
+		function BuscaImagensVeiculo($id){
+			$Auxilio = $this->CarregaHtml('itens/lista-imagens-veiculo-unico-itens');
+			$Sql = "Select * from c_fotos where idveiculo = '".$id."'";
+			$result = $this->Execute($Sql);
+			$num_rows = $this->Linha($result);
+			if ($num_rows){
+				while( $rs = mysql_fetch_array($result , MYSQL_ASSOC) )
+				{
+					if($rs['referencia'] != 0){
+						$Linha = $Auxilio;
+						$Linha = str_replace('<%ID%>',$rs['idveiculo'],$Linha);
+						$Linha = str_replace('<%CAMINHO%>',$rs['caminho'],$Linha);
+						$Linha = str_replace('<%URLPADRAO%>',UrlPadrao,$Linha);
+						$Veiculos .= $Linha; 
+					}
+				}
+			}
+			return $Veiculos;		
 		}
 	}
 ?>
--- lib/veiculo-unico.php
+++ lib/veiculo-unico.php
@@ -11,11 +11,19 @@
 	#Chama funcao Lista Manual passando o Auxilio
 	$VeiculoUnico = $banco->BuscaVeiculoUnico($this->PaginaAux[0]);
 
+	#Carrega imagens de 1 ate 9
+	$ImagensVeiculo = $banco->BuscaImagensVeiculo($this->PaginaAux[0]);
+
 	#Imprimi valores
 	$Conteudo = $banco->CarregaHtml('veiculo-unico');
+	$Conteudo = str_replace('<%IDVEICULO%>',$VeiculoUnico['idveiculo'],$Conteudo);
 	$Conteudo = str_replace('<%MARCA%>',$VeiculoUnico['marca'],$Conteudo);
 	$Conteudo = str_replace('<%MODELO%>',$VeiculoUnico['modelo'],$Conteudo);
 	$Conteudo = str_replace('<%ANOFAB%>',$VeiculoUnico['anofab'],$Conteudo);
 	$Conteudo = str_replace('<%ANOMOD%>',$VeiculoUnico['anomod'],$Conteudo);
 	$Conteudo = str_replace('<%PADRAO%>',$VeiculoUnico['padrao'],$Conteudo);
+	$Conteudo = str_replace('<%URLPADRAO%>',UrlPadrao,$Conteudo);
+	$Conteudo = str_replace('<%CAMINHO%>',$VeiculoUnico['caminho'],$Conteudo);
+	$Conteudo = str_replace('<%IMAGENSVEICULO%>',$ImagensVeiculo,$Conteudo);
+	
 ?>
