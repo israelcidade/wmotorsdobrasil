b7907d7b5d26fc1f99b8d3915ccb394dfdc27d56
Israel Cidade <israelcbj@gmail.com>
2014-11-03 23:30:39 -0200

    coisas

---
+++ functions/banco-log.php
@@ -0,0 +1,31 @@
+<?php
+	class bancolog extends banco{
+		#Funcao que lista os Folders
+
+		function ListaLogs($cpf){
+			$Sql = "Select * from c_log_acesso where cpf = '".$cpf."'";
+			$result = parent::Execute($Sql);
+			$num_rows = parent::Linha($result);
+
+			$Auxilio = parent::CarregaHtml('itens/lista-logs-itens');
+
+			#Monta no Html a Listagem
+			if ($num_rows){
+				while( $rs = mysql_fetch_array($result , MYSQL_ASSOC) )
+				{
+					$Linha = $Auxilio;
+					$Linha = str_replace('<%CPF%>',$cpf,$Linha);
+					$Linha = str_replace('<%IP%>',$rs['ip'],$Linha);
+					$Linha = str_replace('<%DATA%>',$rs['data'],$Linha);
+					$Logs .= $Linha;
+				}
+			}else{
+				return $msg = $this->MontaMsg('atencao',ERRO_ZERO_MARCAS);
+			}
+
+			return $Logs;
+		
+		}
+		
+	}
+?>
---
+++ lib/log-acesso.php
@@ -0,0 +1,18 @@
+<?php
+	#include das funcoes da tela inico
+	include('functions/banco-log.php');
+
+	#Instancia o objeto
+	$banco = new bancolog();
+
+	if($banco->VerificaSessaoAdmin()){
+
+		$LOGS = $banco->ListaLogs($this->PaginaAux[0]));
+
+		#Imprimi valores
+		$Conteudo = $banco->CarregaHtml('log-acesso');
+		$Conteudo = str_replace('<%LOGS%>',$LOGS,$Conteudo);
+	}else{
+		$banco->RedirecionaPara('inicio/acesso');
+	}
+?>
