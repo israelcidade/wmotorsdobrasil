cc016a76b66a7231747fccbb27b6df3443fedde3
Israel Cidade <israelcbj@gmail.com>
2014-11-05 18:40:56 -0200

    mostrando erros = 0

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	ini_set('display_errors',1);
+	/*ini_set('display_errors',1);
 	ini_set('display_startup_erros',1);
-	error_reporting(E_ALL);
+	error_reporting(E_ALL);*/
 	@require('conf/controle.php');
 	#Inicia Controle teste
 	$controle = new controle();
