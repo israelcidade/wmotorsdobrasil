fd8777be9ff560a8fc452be72af1616785716fe4
Israel Cidade <israelcbj@gmail.com>
2014-11-16 20:29:43 -0200

    [FIX] tela admin

--- lib/admin.php
+++ lib/admin.php
@@ -4,19 +4,24 @@
 
 	#Instancia o objeto
 	$banco = new bancoadmin();
-
-	if( isset($_POST["acao"]) && $_POST["acao"] != ''){
-		$user = strip_tags(trim(addslashes($_POST["user"])));
-		$senha = strip_tags(trim(addslashes($_POST["senha"])));
-
-		$flag = $banco->BuscaUsuario($user,$senha);
-
-		if($flag){
-			$banco->IniciaSessaoAdmin($user);
-			$banco->RedirecionaPara('lista-veiculos');
-		}else{
-			$banco->RedirecionaPara('admin');
+	
+	if($banco->VerificaSessaoAdmin()){
+		$banco->RedirecionaPara('lista-veiculos');
+	}else{
+		if( isset($_POST["acao"]) && $_POST["acao"] != ''){
+			$user = strip_tags(trim(addslashes($_POST["user"])));
+			$senha = strip_tags(trim(addslashes($_POST["senha"])));
+		
+			$flag = $banco->BuscaUsuario($user,$senha);
+		
+			if($flag){
+				$banco->IniciaSessaoAdmin($user);
+				$banco->RedirecionaPara('lista-veiculos');
+			}else{
+				$banco->RedirecionaPara('admin');
+			}
 		}
+		$Conteudo = $banco->CarregaHtml('admin');
 	}
-	$Conteudo = $banco->CarregaHtml('admin');
+	
 ?>
