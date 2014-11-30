087b78f7e50d5199ce6ff2d2e13bed8399a4f70f
Israel Cidade <israelcbj@gmail.com>
2014-11-30 16:59:05 -0200

    fazendo recuperar senha

--- functions/banco-recuperar-senha.php
+++ functions/banco-recuperar-senha.php
@@ -8,45 +8,48 @@
 			include("../../app/PHPMailer/class.smtp.php");
 			
 			//Gerar codigo
-			$codigo = $this->geraSenha('10',false,false,false);
 			if($usuario = $this->buscaUsuarioPorEmail($email)){
+				$codigo = $this->geraSenha('10',false,false,false);
 				
+				//Salva codigo no banco com o id do usuario
+				$SqlSalvaCodigo = "Insert into c_codigos (idusuario,codigo) VALUES ('".$usuario['idusuario']."', '".$codigo."')";
+				$reuulstInter = $this->Execute($SqlSalvaCodigo);
+				
+				$mail = new PHPMailer();
+				// Charset para evitar erros de caracteres
+				$mail->Charset = 'UTF-8';
+				// Dados de quem est� enviando o email
+				$mail->From = 'contato@wmotorsdobrasil.com';
+				$mail->FromName = 'wmotorsdobrasil';
+					
+				// Setando o conteudo
+				$mail->IsHTML(true);
+				$mail->Subject = 'WmotorsDoBrasil -> Recuperação de Senha';
+				$mail->Body = utf8_decode(
+						'Clique no link a baixo para recuperar sua senha.<br>
+					<a href="'.UrlPadrao.'recuperar-senha/codigo/$codigo">$codigo</a>'
+				);
+					
+				// Validando a autentica��o
+				$mail->IsSMTP();
+				$mail->SMTPAuth = true;
+				$mail->Host     = "ssl://smtp.gmail.com";
+				$mail->Port     = 465;
+				$mail->Username = EMAIL_USER;
+				$mail->Password = EMAIL_PASS;
+					
+				// Setando o endere�o de recebimento
+				$mail->AddAddress(EMAIL_RECEB);
+					
+				// Enviando o e-mail para o usu�rio
+				if($mail->Send()){
+					echo 'ok';
+				}else{
+					echo 'false';
+				}	
 			}else{
-				return false;
-			}
-			
-			$mail = new PHPMailer();
-			// Charset para evitar erros de caracteres
-			$mail->Charset = 'UTF-8';
-			// Dados de quem est� enviando o email
-			$mail->From = 'contato@wmotorsdobrasil.com';
-			$mail->FromName = 'wmotorsdobrasil';
-			
-			// Setando o conteudo
-			$mail->IsHTML(true);
-			$mail->Subject = 'WmotorsDoBrasil -> Bem Vindo';
-			$mail->Body = utf8_decode(
-					'Bem Vindo ao Wmotors do Brasil!<br>
-					Realize seu pagamento e come�e a utilizar nosso site para suas pesquisas!'
-			);
-			
-			// Validando a autentica��o
-			$mail->IsSMTP();
-			$mail->SMTPAuth = true;
-			$mail->Host     = "ssl://smtp.gmail.com";
-			$mail->Port     = 465;
-			$mail->Username = EMAIL_USER;
-			$mail->Password = EMAIL_PASS;
-			
-			// Setando o endere�o de recebimento
-			$mail->AddAddress($email);
-			
-			// Enviando o e-mail para o usu�rio
-			if($mail->Send()){
-				return true;
-			}else{
-				return false;
-			}
+				echo 'false';
+			}	
 		}
 		
 		function buscaUsuarioPorEmail($email){
