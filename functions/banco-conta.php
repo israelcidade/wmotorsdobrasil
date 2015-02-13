063abe531cb5bb3632e4b69a5d82b9dcb0658f6b
Israel Cidade <israelcbj@gmail.com>
2015-02-13 01:13:15 -0200

    fix atualizar endereco

--- functions/banco-conta.php
+++ functions/banco-conta.php
@@ -1,14 +1,25 @@
 <?php
 	class bancoconta extends banco{
 		
-		/*function BuscaUsuario($user,$senha){
-			$Sql = "Select * from c_usuarios where nome = '".$user."' AND senha = '".$senha."' ";
-			$result = parent::Execute($Sql);
-			$num_rows = parent::Linha($result);
-			return $num_rows;
-			}
-			*/
 		
+		function BuscaInfoUsuario(){
+			$Sql = "Select * from c_usuarios where cpf = '".$_SESSION['cpf']."' ";
+			$result = parent::Execute($Sql);
+			$rs = mysql_fetch_array($result , MYSQL_ASSOC);
+			return $rs;
+		}
+		
+		function AtualizarEndereco($end){
+			$Sql = "Update c_usuarios set 
+					cep = '".$end['cep']."', 
+					endereco = '".$end['endereco']."',
+					bairro = '".$end['bairro']."',
+					cidade = '".$end['cidade']."',
+					estado = '".$end['estado']."'
+					where cpf = '".$_SESSION['cpf']."' " ;
+			$result = parent::Execute($Sql);
+			return true;
+		}
 		
 	}
 ?>
--- lib/conta.php
+++ lib/conta.php
@@ -7,10 +7,36 @@
 
 	if($banco->VerificaSessao()){
 
+		$info = $banco->BuscaInfoUsuario();
 		
-
+		if( isset($_POST["acao"]) && $_POST["acao"] != '' ){
+			$endereco['cep'] = strip_tags(trim(addslashes($_POST["cep"])));
+			$endereco['endereco'] = strip_tags(trim(addslashes($_POST["endereco"])));
+			$endereco['bairro'] = strip_tags(trim(addslashes($_POST["bairro"])));
+			$endereco['cidade'] = strip_tags(trim(addslashes($_POST["cidade"])));
+			$endereco['estado'] = strip_tags(trim(addslashes($_POST["estado"])));
+			
+			$banco->AtualizarEndereco($endereco);
+			$banco->RedirecionaPara('conta');
+			
+		}
+		
 		#Imprime Valores
 		$Conteudo = $banco->CarregaHtml('conta');
+		$Conteudo = str_replace('<%NOME%>',$info['nome'],$Conteudo);
+		$Conteudo = str_replace('<%DATANASC%>',date('d/m/Y', strtotime($info['nascimento'])),$Conteudo);
+		$Conteudo = str_replace('<%EMAIL%>',$info['email'],$Conteudo);
+		$Conteudo = str_replace('<%CPF%>',$info['cpf'],$Conteudo);
+		$Conteudo = str_replace('<%RG%>',$info['rg'],$Conteudo);
+		
+		//Endereco
+		$Conteudo = str_replace('<%CEP%>',$info['cep'],$Conteudo);
+		$Conteudo = str_replace('<%ENDERECO%>',$info['endereco'],$Conteudo);
+		$Conteudo = str_replace('<%BAIRRO%>',$info['bairro'],$Conteudo);
+		$Conteudo = str_replace('<%CIDADE%>',$info['cidade'],$Conteudo);
+		$Conteudo = str_replace('<%ESTADO%>',$info['estado'],$Conteudo);
+		
+		
 		//$Conteudo = str_replace('<%BUSCA%>',$Busca,$Conteudo);
 
 	}else{
