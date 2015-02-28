bb0147dcbf74ae6869ee52d7192cd8902e8231c6
Israel Cidade <israelcbj@gmail.com>
2015-02-28 09:37:11 -0300

    fix permissão acesso paginas

--- lib/parceiros.php
+++ lib/parceiros.php
@@ -5,13 +5,10 @@
 	#Instancia o objeto
 	$banco = new bancoparceiros();
 	
-	if($banco->VerificaSessao()){
+	
 		#Declara Variaveis
 		$msg  = '';
 	
 		$Conteudo = $banco->CarregaHtml('parceiros');
 		$Conteudo = str_replace('<%MSG%>',$msg,$Conteudo);
-	}else{
-		$banco->RedirecionaPara('inicio/acesso');
-	}
-?>
+	?>
--- lib/recuperadores.php
+++ lib/recuperadores.php
@@ -5,9 +5,14 @@
 	#Instancia o objeto
 	$banco = new bancoparceiros();
 	
+	
+	if($banco->VerificaSessao()){
 	//$sobre = $banco->SelectSobre();
 	
 	
 	$Conteudo = $banco->CarregaHtml('recuperadores');
 	//$Conteudo = str_replace('<%SOBRE%>', $sobre, $Conteudo);
+	}else{
+		$banco->RedirecionaPara('inicio/acesso');
+	}
 ?>
